e3b29b9815b76f93aee22fd8ba56d265de25765e
my pc <my pc@DESKTOP-SGIKIS6>
2022-02-21 15:57:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int num, rem, sum = 0, temp;
+          
+        Console.WriteLine("\n >>>> To Find the sum a number <<<< ");
+        Console.Write("\n Enter a number: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        
+        while (num > 0)
+        {
+            rem = num % 10;   
+            sum = sum  + rem;
+            num = num / 10;
+            
+        }
+        Console.WriteLine("\n The Sum of Number is: {0} \n", sum);
+       
+    }
+}
